2f7c473e76a9a09d1af7c7af4b69ac60db4a1d26
Oleg Lontsov <oleglontsov@gmail.com>
2012-01-05 21:11:16 +0400

    Alpha version.

--- server.py
+++ server.py
@@ -1,1 +1,55 @@
-__author__ = 'Scream'
+import socketserver
+import sys
+import time
+import substitutor
+import threading
+
+dict = {}
+sleepTime = 0
+rlock = threading.RLock()
+
+
+class ThreadedTCPRequestHandler(socketserver.StreamRequestHandler):
+    def handle(self):
+        global dict, sleepTime, rlock
+        self.data = self.rfile.readline().strip()
+        req = (self.data.decode()).split(' ', 2)
+        length = len(req)
+        if req[0] == 'GET':
+            time.sleep(sleepTime)
+            if length >= 2:
+                self.wfile.write(('VALUE\n'+substitutor.get(dict, req[1])).encode())
+            else:
+                self.wfile.write('GET syntax: GET key'.encode())
+        elif req[0] == 'SET' and req[1] == 'SLEEP':
+            if length == 3:
+                rlock.acquire()
+                sleepTime = int(req[2])
+                rlock.release()
+                self.wfile.write('OK'.encode())
+            else:
+                self.wfile.write('SET syntax: SET SLEEP sleeptime')
+        elif req[0] == 'PUT':
+            time.sleep(sleepTime)
+            if length == 3:
+                rlock.acquire()
+                substitutor.put(dict, req[1], req[2])
+                rlock.release()
+                self.wfile.write('OK'.encode())
+            else:
+                self.wfile.write('PUT syntax: PUT key value'.encode()
+)
+        else:
+            self.wfile.write('Unsupported request! We accept GET, PUT or SET SLEEP here!'.encode()
+)
+
+
+if __name__ == "__main__":
+    try:
+        HOST, PORT = sys.argv[1:]
+    except:
+        HOST, PORT = "127.0.0.1", 1298
+    server = socketserver.ThreadingTCPServer((HOST, PORT), ThreadedTCPRequestHandler)
+    server.serve_forever()
+
+
--- substitutor.py
+++ substitutor.py
@@ -1,1 +1,24 @@
-__author__ = 'Scream'
+import re
+
+inProgress = set()
+
+
+def put(dict, key, value):
+    dict[key] = value
+
+def get(dict, key):
+    global inProgress
+    if key in inProgress:
+        inProgress.clear()
+        return 'Panic!!! Infinite recursion!!!'
+    if key in dict:
+        out = dict[key]
+    else: return ''
+    inProgress.add(key)
+    for templ in set(re.findall(r'\$\{(\S+)\}', out)):
+        part = get(dict, templ)
+        if part == 'Panic!!! Infinite recursion!!!':
+            return part
+        out = out.replace('${' + templ + '}', part)
+    inProgress.remove(key)
+    return out
